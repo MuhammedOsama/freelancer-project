ec114b2e4cca953d8a4d89f80511891271d9d83d
Muhammed Osama <60500495+MuhammedOsama@users.noreply.github.com>
2020-05-16 17:11:57 +0200

    Create Navbar

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import {Nav, Navbar} from 'react-bootstrap';
+
+class Header extends Component {
+    render() {
+    return(
+        <div className="container-fluid">
+            <Navbar collapseOnSelect expand="lg">
+                <img src="/images/logo.svg" alt="Logo"/>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link className="works" href="#">How It Works</Nav.Link>
+                        <Nav.Link className="jobs" href="#">Browse Jobs</Nav.Link>
+                    </Nav>
+                    <Nav className="links">
+                        <Nav.Link className="login" href="#">Log In</Nav.Link>
+                        <Nav.Link className="signup" href="#">Sign Up</Nav.Link>
+                        <button value="submit">Post a Project</button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+  }
+}
+
+export default Header;
