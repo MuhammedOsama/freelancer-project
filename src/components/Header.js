74624f7992ab688b003a6c7dca35f8bfea60f13a
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-05-21 17:56:26 +0200

    MainCarousel Refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Button } from 'react-bootstrap';
 
 class Header extends Component {
     render() {
     return(
-        <div className="container">
+        <div id="header" className="container">
             <Navbar collapseOnSelect expand="lg">
                 <img src="/images/logo/logo.svg" alt="Logo"/>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto nav-links">
                         <Nav.Link className="nav-works-btn" href="#">How It Works</Nav.Link>
-                        <Nav.Link className="jobs" href="#">Browse Jobs</Nav.Link>
+                        <Nav.Link className="nav-jobs-btn" href="#">Browse Jobs</Nav.Link>
                     </Nav>
                     <Nav className="links">
                         <Nav.Link className="login" href="#">Log In</Nav.Link>
                         <Nav.Link className="signup" href="#">Sign Up</Nav.Link>
-                        <button value="submit">Post a Project</button>
+                        <button>Post a Project</button>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
--- src/components/MainCarousel.js
+++ src/components/MainCarousel.js
@@ -1,69 +1,32 @@
-import React, { Component } from 'react';
-import { Carousel } from 'react-bootstrap';
+import React from 'react';
+import { Carousel, CarouselItem } from 'react-bootstrap';
 
-class MainCarousel extends Component {
-    render() {
-        return(
-            <div>
-                <Carousel className="expert">
-                    <Carousel.Item className="bg-cover">
-                        <img src="/images/carousel/mobile.jpg" alt="Mobile"/>
-                        <Carousel.Caption>
-                            <h1 className="caption_title">Hire expert freelancers for any job, online</h1>
-                            <p>Millions of small businesses use Freelancer to turn their ideas into reality..</p>
-                            <button className="hire" value="submit">I want to Hire</button>
-                            <button className="work" value="submit">I want to Work</button>
-                            <div className="price">
-                                <span>This Mobile App Design Cost </span>
-                                <label>$150</label>
-                            </div>
-                        </Carousel.Caption>
-                    </Carousel.Item>
+class MainCarousel extends React.Component{
+  render (){
+        return (
+            <div className="main-carousel d-none d-md-block">
+              <div className="main-carousel-caption">
+                  <h1>Hire expert freelancers for any job, online</h1>
+                  <p className="lead">Millions of small businesses use Freelancer to turn their ideas into reality.</p>
+                  <button>I want Hire</button>
+                  <button>I want Work</button>
+              </div>
+              <Carousel>
+                <Carousel.Item>
+                <img className="d-block w-100" src="/images/carousel/bg2.jpg" alt="First Image" />
+                </Carousel.Item>
 
-                    <Carousel.Item className="bg-cover">
-                        <img src="/images/carousel/architectural.jpg" alt="Architectural"/>
-                        <Carousel.Caption>
-                            <h1 className="caption_title">Hire expert freelancers for any job, online</h1>
-                            <p>Millions of small businesses use Freelancer to turn their ideas into reality..</p>
-                            <button className="hire" value="submit">I want to Hire</button>
-                            <button className="work" value="submit">I want to Work</button>
-                            <div className="price">
-                                <span>This Architectural Design Cost </span>
-                                <label>$200</label>
-                            </div>
-                        </Carousel.Caption>
-                    </Carousel.Item>
+                <Carousel.Item>
+                  <img className="d-block w-100" src="/images/carousel/bg1.jpg" alt="Second Image" />
+                </Carousel.Item>
 
-                    <Carousel.Item className="bg-cover">
-                        <img src="/images/carousel/nasa.jpg" alt="Nasa"/>
-                        <Carousel.Caption>
-                            <h1 className="caption_title">Hire expert freelancers for any job, online</h1>
-                            <p>Millions of small businesses use Freelancer to turn their ideas into reality..</p>
-                            <button className="hire" value="submit">I want to Hire</button>
-                            <button className="work" value="submit">I want to Work</button>
-                            <div className="price">
-                                <span>This Nasa Design Cost </span>
-                                <label>$250</label>
-                            </div>
-                        </Carousel.Caption>
-                    </Carousel.Item>
+                <Carousel.Item>
+                  <img className="d-block w-100" src="/images/carousel/bg2.jpg" alt="Third Image" />
+                </Carousel.Item>
 
-                    <Carousel.Item className="bg-cover">
-                        <img src="/images/carousel/website.jpg" alt="Website"/>
-                        <Carousel.Caption>
-                            <h1 className="caption_title">Hire expert freelancers for any job, online</h1>
-                            <p>Millions of small businesses use Freelancer to turn their ideas into reality..</p>
-                            <button value="submit">I want to Hire</button>
-                            <button value="submit">I want to Work</button>
-                            <div className="price">
-                                <span>This Website Design Cost </span>
-                                <label>$300</label>
-                            </div>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+              </Carousel>
             </div>
-        );
+      );
     }
 }
 
