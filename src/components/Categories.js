c4948a6315b093c256b86f2208050191bdd04295
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-06-08 11:32:15 +0200

    Update Categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,162 +1,65 @@
 import React, { Component } from 'react';
 
 class Categories extends Component {
+    // todo: get Categories from api
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {image: "/images/categories/hire-php.svg", title: "Title", description: "PHP"},
+                {image: "/images/categories/hire-website-design.svg", title: "Title", description: "Website"},
+                {image: "/images/categories/hire-graphic-design.svg", title: "Title", description: "Graphic"},
+                {image: "/images/categories/hire-mobile-apps.svg", title: "Title", description: "Mobile"},
+                {image: "/images/categories/hire-android.svg", title: "Title", description: "Andriod"},
+                {image: "/images/categories/hire-iphone-apps.svg", title: "Title", description: "iPhone"},
+                {image: "/images/categories/hire-photoshop.svg", title: "Title", description: "Photoshop"},
+                {image: "/images/categories/hire-logo-design.svg", title: "Title", description: "Logo"},
+                {image: "/images/categories/hire-article-writing.svg", title: "Title", description: "Article"},
+                {image: "/images/categories/hire-data-entry.svg", title: "Title", description: "Data"},
+                {image: "/images/categories/hire-internet-marketing.svg", title: "Title", description: "Internet"},
+                {image: "/images/categories/hire-seo.svg", title: "Title", description: "SEO"},
+                {image: "/images/categories/hire-php.svg", title: "Title", description: "PHP"},
+                {image: "/images/categories/hire-website-design.svg", title: "Title", description: "Website"},
+                {image: "/images/categories/hire-graphic-design.svg", title: "Title", description: "Graphic"},
+                {image: "/images/categories/hire-mobile-apps.svg", title: "Title", description: "Mobile"},
+                {image: "/images/categories/hire-android.svg", title: "Title", description: "Andriod"},
+                {image: "/images/categories/hire-iphone-apps.svg", title: "Title", description: "iPhone"},
+                {image: "/images/categories/hire-photoshop.svg", title: "Title", description: "Photoshop"},
+                {image: "/images/categories/hire-logo-design.svg", title: "Title", description: "Logo"},
+                {image: "/images/categories/hire-article-writing.svg", title: "Title", description: "Article"},
+                {image: "/images/categories/hire-data-entry.svg", title: "Title", description: "Data"},
+                {image: "/images/categories/hire-internet-marketing.svg", title: "Title", description: "Internet"},
+                {image: "/images/categories/hire-seo.svg", title: "Title", description: "SEO"},
+                {image: "/images/categories/hire-php.svg", title: "Title", description: "PHP"},
+                {image: "/images/categories/hire-website-design.svg", title: "Title", description: "Website"},
+                {image: "/images/categories/hire-graphic-design.svg", title: "Title", description: "Graphic"},
+                {image: "/images/categories/hire-mobile-apps.svg", title: "Title", description: "Mobile"},
+                {image: "/images/categories/hire-android.svg", title: "Title", description: "Andriod"},
+                {image: "/images/categories/hire-iphone-apps.svg", title: "Title", description: "iPhone"},
+                {image: "/images/categories/hire-photoshop.svg", title: "Title", description: "Photoshop"},
+                {image: "/images/categories/hire-logo-design.svg", title: "Title", description: "Logo"},
+                {image: "/images/categories/hire-article-writing.svg", title: "Title", description: "Article"},
+                {image: "/images/categories/hire-data-entry.svg", title: "Title", description: "Data"},
+                {image: "/images/categories/hire-internet-marketing.svg", title: "Title", description: "Internet"},
+                {image: "/images/categories/hire-seo.svg", title: "Title", description: "SEO"},
+            ]
+        }
+    }
+
     render() {
         return(
             <div className="categories">
                 <h2>Get work done in over 1350 different categories</h2>
                 <div className="container text-center">
                     <div className="row">
-                        <div className="col ph">
-                            <img src="/images/categories/hire-php.svg" alt="Php"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-website-design.svg" alt="Website"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-graphic-design.svg" alt="Graphic"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-mobile-apps.svg" alt="Mobile"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-android.svg" alt="Android"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-iphone-apps.svg" alt="iPhone"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-photoshop.svg" alt="Photoshop"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-logo-design.svg" alt="Logo"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-article-writing.svg" alt="Article"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-data-entry.svg" alt="Data"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-internet-marketing.svg" alt="Internet"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-seo.svg" alt="Seo"/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col text">
-                            <a href="#">PHP</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Website Design</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Graphic Design</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Mobile Apps</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Android Apps</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">iPhone Apps</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Photoshop</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Logo Design</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Article Writing</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Data Entry</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Internet Marketing</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">SEO</a>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <img src="/images/categories/hire-3d-modeling.svg" alt="Modeling"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-copy-writing.svg" alt="Copy"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-ghost-writing.svg" alt="Ghost"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-linux.svg" alt="Linux"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-aws.svg" alt="Amazon"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-banner-design.svg" alt="Banner"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-software-dev.svg" alt="Software"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-accounting.svg" alt="Accounting"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-finance.svg" alt="Finance"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-legal.svg" alt="Legal"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-manufacturing.svg" alt="Manufacturing"/>
-                        </div>
-                        <div className="col">
-                            <img src="/images/categories/hire-logistics.svg" alt="Logistics"/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <a href="#">Modeling</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Copy</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Ghost</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Linux</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Amazon</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Banner</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Software</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Accounting</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Finance</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Legal</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Manufacturing</a>
-                        </div>
-                        <div className="col">
-                            <a href="#">Logistics</a>
-                        </div>
+                        {this.state.items.map((item, id) => {
+                            return (
+                                <div key={id} className="col-1 mb-5">
+                                    <img src={item.image} alt={item.description} />
+                                    <a href="/">{item.title}</a>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </div>
